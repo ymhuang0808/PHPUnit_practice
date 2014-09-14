23a76f9cdb82a453504a9feb83efef3bfe85f0fd
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-09-14 15:27:35 +0800

    Add @expectedExceptionMessage and @expectedExceptionCode in test/ExceptionTest.php

--- test/ExceptionTest.php
+++ test/ExceptionTest.php
@@ -7,4 +7,22 @@ class ExceptionTest extends PHPUnit_Framework_TestCase
 	public function testException()
 	{
 	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Right message
+	 */
+	public function testExceptionHasRightMessage()
+	{
+		throw new InvalidArgumentException('Some messages', 20);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionCode 20
+	 */
+	public function testExceptionCode()
+	{
+		throw new InvalidArgumentException('Right message', 10);
+	}
 }
